1f1b91558b5f83903e17e90239eef405b0f04cf0
ttlienn4902 <101441307+ttlienn4902@users.noreply.github.com>
2022-05-11 21:07:06 +0700

    Add files via upload

---
+++ ketnoicsdl/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace ketnoicsdl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=LAPTOP-OV1HU3M9\\SQLEXPRESS;" + "Initial Catalog=Quanlybanhang;" + "Integrated Security=True";
+            try
+            {
+                con.Open();
+                MessageBox.Show("kết nối csdl thành công");
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("KET NOI CSDL FAIL" + ex.ToString());
+            }
+            }
+        }
+    }
+
